477f763178a57de4210a8902b40933ef86ad5aa4
iunctis <steven.abgrall@gmail.com>
2018-11-29 01:24:59 +0100

    Compatibilité FA5

--- plugin.rb
+++ plugin.rb
@@ -7,3 +7,11 @@
 enabled_site_setting :fmstlb_ui_enabled
 
 register_asset 'stylesheets/fmstlb.scss'
+
+register_svg_icon "fa-underline" if respond_to?(:register_svg_icon)
+register_svg_icon "far-newspaper" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-indent" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-center" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-right" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-justify" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-font" if respond_to?(:register_svg_icon)
